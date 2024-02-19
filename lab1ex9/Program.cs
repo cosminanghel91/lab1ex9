bfd60ae9e04f89660d629515ac1643749b110b43
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:52:58 +0200

    Add project files.

---
+++ lab1ex9/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+* Scrieti un program care va interschimba valorile la 2 numere intregi
+*/
+
+
+Console.WriteLine("Introduceti valoarea pentru primul numar:");
+int numar1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru al doilea numar:");
+int numar2 = int.Parse(Console.ReadLine());
+
+int temporar = numar2;
+numar2 = numar1;
+numar1 = temporar;
+
+Console.WriteLine("Valoarea noua primului numar este:" + numar1 + " iar valoarea noua pentru al doilea numar este:" + numar2);
